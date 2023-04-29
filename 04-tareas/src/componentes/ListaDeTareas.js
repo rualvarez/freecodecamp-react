8cc9365e47868bdec069a6caf848c141984e41c7
Ruben Alvarez <dev.ralvarez@gmail.com>
2023-04-29 17:01:01 +0200

    Manjear estado de la tarea que se introduce y mandar la tarea creada en el formulario al parent con props.onSubmit

--- 04-tareas/src/componentes/ListaDeTareas.js
+++ 04-tareas/src/componentes/ListaDeTareas.js
@@ -1,13 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
 import '../hojas-de-estilo/ListaDeTareas.css'
 import TareaFormulario from "./TareaFormulario";
+import Tarea from "./Tarea";
 
 function ListaDeTareas() {
+
+  const[tareas, setTareas] = useState([]);
+
+  const agregarTarea = tarea => {
+    console.log('agregada', tarea);
+  }
+
   return (
     <>
       <TareaFormulario />
       <div className="tareas-lista-contenedor">
-        LISTA DE TAREAS
+        {
+          tareas.map((tarea) =>
+            <Tarea 
+              texto={tarea.texto}
+              completada={tarea.completada}
+            />
+          )
+        }
       </div>
     </>
   );
--- 04-tareas/src/componentes/TareaFormulario.js
+++ 04-tareas/src/componentes/TareaFormulario.js
@@ -1,14 +1,38 @@
-import React from 'react';
+import React, { useState } from 'react';
 import '../hojas-de-estilo/TareaFormulario.css';
+import { v4 as uuidv4 } from 'uuid';
 
 function TareaFormulario(props) {
+
+  const [input, setInput] = useState('');
+
+  const manejarCambio = e => {
+    setInput(e.target.value);
+  }
+
+  const manejarEnvio = e => {
+    e.preventDefault(); //No volver a cargar toda la aplicacion cuando se envie el formulario
+
+    const tareaNueva = {
+      id: uuidv4(),
+      texto: input,
+      completada: false
+    }
+
+    props.onSubmit(tareaNueva);
+  }
+
   return (
-    <form className='tarea-formulario'>
+    <form 
+      className='tarea-formulario'
+      onSubmit={manejarEnvio}
+    >
       <input 
         className='tarea-input'
         type='text'
         placeholder='Escribe una Tarea'
         name='texto'
+        onChange={manejarCambio}
       />
       <button className='tarea-boton'>
         Agregar Tarea
