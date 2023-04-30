4c4a8db39abefd198820162a7a3f0da1a55d769c
Ruben Alvarez <dev.ralvarez@gmail.com>
2023-04-30 16:46:42 +0200

    Convertir componente App de funcional a componente de clase

--- 06-contador-clicks-clase/src/App.js
+++ 06-contador-clicks-clase/src/App.js
@@ -2,42 +2,86 @@ import './App.css';
 import Boton from './componentes/Boton';
 import Contador from './componentes/Contador';
 import freeCodeCampLogo from './imagenes/fcc_primary.svg';
-import { useState } from 'react';
+import React from 'react';
 
-function App() {
-
-  const [numClicks, setNumClicks] = useState(0);
-
-  const manejarClick = () => {
-    setNumClicks(numClicks + 1);
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      numClicks: 0
+    };
+    this.manejarClick = this.manejarClick.bind(this);
+    this.reiniciarContador = this.reiniciarContador.bind(this);
   }
 
-  const reiniciarContador = () => {
-    setNumClicks(0);
+  manejarClick() {
+    this.setState(({ numClicks }) => ({ numClicks: numClicks + 1 }));
   }
 
-  return (
-    <div className='App'>
-      <div className='freecodecamp-logo-contenedor'>
-        <img 
-          className='freecodecamp-logo'
-          src={freeCodeCampLogo}
-          alt='Logo de freeCodeCamp' />
+  reiniciarContador() {
+    this.setState({ numClicks: 0 });
+  }
+
+  render() {
+    return (
+      <div className='App'>
+        <div className='freecodecamp-logo-contenedor'>
+          <img 
+            className='freecodecamp-logo'
+            src={freeCodeCampLogo}
+            alt='Logo de freeCodeCamp' />
+        </div>
+        <div className='contenedor-principal'>
+          <Contador 
+            numClicks={this.state.numClicks} />
+          <Boton 
+            texto='Click'
+            esBotonDeClick={true}
+            manejarClick={this.manejarClick} />
+          <Boton 
+            texto='Reiniciar'
+            esBotonDeClick={false}
+            manejarClick={this.reiniciarContador} />
+        </div>
       </div>
-      <div className='contenedor-principal'>
-        <Contador 
-          numClicks={numClicks} />
-        <Boton 
-          texto='Click'
-          esBotonDeClick={true}
-          manejarClick={manejarClick} />
-        <Boton 
-          texto='Reiniciar'
-          esBotonDeClick={false}
-          manejarClick={reiniciarContador} />
-      </div>
-    </div>
-  );
+    );
+  }
 }
+
+// function App() {
+
+//   const [numClicks, setNumClicks] = useState(0);
+
+//   const manejarClick = () => {
+//     setNumClicks(numClicks + 1);
+//   }
+
+//   const reiniciarContador = () => {
+//     setNumClicks(0);
+//   }
+
+//   return (
+//     <div className='App'>
+//       <div className='freecodecamp-logo-contenedor'>
+//         <img 
+//           className='freecodecamp-logo'
+//           src={freeCodeCampLogo}
+//           alt='Logo de freeCodeCamp' />
+//       </div>
+//       <div className='contenedor-principal'>
+//         <Contador 
+//           numClicks={numClicks} />
+//         <Boton 
+//           texto='Click'
+//           esBotonDeClick={true}
+//           manejarClick={manejarClick} />
+//         <Boton 
+//           texto='Reiniciar'
+//           esBotonDeClick={false}
+//           manejarClick={reiniciarContador} />
+//       </div>
+//     </div>
+//   );
+// }
 
 export default App;
