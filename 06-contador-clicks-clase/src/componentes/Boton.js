a9628f13d4eb7b70d709016f8510574b7eb3928e
Ruben Alvarez <dev.ralvarez@gmail.com>
2023-04-30 16:30:57 +0200

    Convertir componente Boton de funcional a componente de clase

--- 06-contador-clicks-clase/src/componentes/Boton.js
+++ 06-contador-clicks-clase/src/componentes/Boton.js
@@ -1,14 +1,16 @@
 import React from 'react';
 import '../hojas-de-estilo/Boton.css'
 
-function Boton({ texto, esBotonDeClick, manejarClick }) {
-  return (
-    <button
-      className={esBotonDeClick ? 'boton-click' : 'boton-reiniciar'}
-      onClick={manejarClick}>
-      {texto}
-    </button>
-  )
+class Boton extends React.Component {
+  render() {
+    return (
+      <button
+        className={this.props.esBotonDeClick ? 'boton-click' : 'boton-reiniciar'}
+        onClick={this.props.manejarClick}>
+        {this.props.texto}
+      </button>
+    );
+  }
 }
 
 export default Boton;
